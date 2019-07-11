7b4837bfd196fac43e32b16cb4bc8a3faca1c5f9
Daubiht <daubihtmania@gmail.com>
2019-07-11 12:02:35 +0200

    Liaison back et front effective

--- Cocktail/Cocktail/Program.fs
+++ Cocktail/Cocktail/Program.fs
@@ -1,8 +1,34 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
+open System.Threading
+open Suave
+open Suave.Filters
+open Suave.Operators
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let cts = new CancellationTokenSource()
+    let sourceDir = __SOURCE_DIRECTORY__
+    let folder = "\Front"
+    let conf = { defaultConfig with cancellationToken = cts.Token ; homeFolder = Some (sourceDir + folder) }  
+
+    let app =
+        choose [
+            GET >=> choose
+                [ path "/" >=> Files.browseFileHome "Index.html"
+                ]
+            POST >=> choose
+                [ path "/" >=> Files.browseFileHome "Index.html"
+                ]
+            GET >=> Files.browseHome
+            RequestErrors.NOT_FOUND "Page not found."  
+        ]
+
+    let listening, server = startWebServerAsync conf (app)
+    
+    Async.Start(server, cts.Token)
+    printfn "Make requests now"
+    Console.ReadKey true |> ignore
+    
+    cts.Cancel()
+
+    0
