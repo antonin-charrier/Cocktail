199e7c2c3b6a74b68be375d34026a967d88de9af
Benoit Humblot <benoit.humblot@orange.fr>
2019-07-12 15:14:06 +0200

    file write

---
+++ Cocktail/Cocktail/Persist.fs
@@ -0,0 +1,28 @@
+﻿module Persist
+
+open Suave
+open Suave.Json
+open System.Runtime.Serialization
+
+[<DataContract>]
+type Color
+    = White
+    | Black
+
+[<DataContract>]
+type Piece
+    = King
+    | Queen
+    | Bishop
+    | Knight
+    | Rook
+    | Pawn
+
+let testlist = ["King"; "Queen"; "Pawn";]
+
+let save piecelist = 
+    use file = System.IO.File.CreateText("C:\Dev\Prog_Fonct\save")
+    piecelist |> List.iter(fun elem -> fprintf file "%s " elem+"/n")
+    fprintfn file ""
+
+save testlist
